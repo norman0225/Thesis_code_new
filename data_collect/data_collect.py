8954c79024973d1db18cb6098a2fb8047e482e86
norman0225 <50372404+norman0225@users.noreply.github.com>
2022-06-08 14:19:26 +0800

    Create data_collect.py

---
+++ data_collect/data_collect.py
@@ -0,0 +1,36 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
+import reading_IMU
+import pandas as pd
+import time
+
+def arm():
+    
+    print("Basic pre-arm checks")
+    #Do not let user try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print("Waiting for vehicle initialise...")
+        time.sleep(1)
+    
+    print("Arming motors")
+    #Copter should arm in GUIDED mode
+    #vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+    
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+     
+        
+date = input("please enter the date and time(2022_03_02_12:30):")
+
+vehicle = dronekit.connect('/dev/ttyS0',wait_ready=True,baud=921600) #connect to vehicel
+
+sample = []
+while vehicle.armed:
+  sample.append(reading_IMU.Berry_conv_acc_list())
+
+name = ['t','AccX','AccY']
+f = pd.DataFrame(columns=name,data=sample)
+f.to_csv('list_test.csv', mode='w', float_format='%f', header=False, index=0)
+
+print('done')
