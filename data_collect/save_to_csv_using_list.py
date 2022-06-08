753ac2ebae01da0b92845457280aa77c3dad52e2
norman0225 <50372404+norman0225@users.noreply.github.com>
2022-06-08 14:18:38 +0800

    Create save_to_csv_using_list.py

---
+++ data_collect/save_to_csv_using_list.py
@@ -0,0 +1,21 @@
+import reading_IMU
+import pandas as pd
+import time
+
+
+start = time.time()
+sample = []
+for i in range(40000):
+  
+  t = time.time()
+  sample.append(reading_IMU.Berry_conv_acc_list())
+
+end1 = time.time()
+name = ['t','X','Y']
+f = pd.DataFrame(columns=name,data=sample)
+f.to_csv('list_test.csv', mode='w', float_format='%f', header=False, index=0)
+end2 = time.time()
+
+print('done')
+print(end1-start)
+print('40,000 data spent',end2-start,'sec,using list and Berry')
